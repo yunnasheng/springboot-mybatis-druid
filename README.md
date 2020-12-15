ba8aede11427e58150f1ea63b5688b8be7c68ef1
yunnasheng <yunnasheng@payeasenet.com>
2020-12-15 19:09:57 +0800

    add lombok dependency

--- README.md
+++ README.md
@@ -5,3 +5,18 @@
 > * Logback 
 > * MyBatis
 > * MySQL
+
+* 建表语句
+
+```
+-- 创建表 -- 
+CREATE TABLE `lt_dict` (
+  `id` varchar(64) NOT NULL,
+  `code` varchar(128) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `status` varchar(12) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ;
+-- 插入数据 --
+INSERT INTO lt_dict (id, code, name, status) VALUES ('3c3da1d4b3b94bd9b33fb340dfdb605c', '001', 'cjack-60', '0');
+```
--- src/main/java/com/lb/springboot/config/datasource/DruidDataSourceProperties.java
+++ src/main/java/com/lb/springboot/config/datasource/DruidDataSourceProperties.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Component;
 
+import lombok.Getter;
+import lombok.Setter;
+
 
 /**
  * <p>
@@ -23,6 +26,8 @@ import org.springframework.stereotype.Component;
 @Component
 @ConfigurationProperties(prefix = "spring.datasource")
 @PropertySource("classpath:druid.properties")
+@Getter
+@Setter
 public class DruidDataSourceProperties {
 
 	/**
@@ -78,141 +83,5 @@ public class DruidDataSourceProperties {
 	@Bean
 	public static PropertySourcesPlaceholderConfigurer propertyConfigure(){
 		return new PropertySourcesPlaceholderConfigurer();
-	}
-
-	public String getDriverClassName() {
-		return driverClassName;
-	}
-
-	public void setDriverClassName(String driverClassName) {
-		this.driverClassName = driverClassName;
-	}
-
-	public String getUrl() {
-		return url;
-	}
-
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public int getInitialSize() {
-		return initialSize;
-	}
-
-	public void setInitialSize(int initialSize) {
-		this.initialSize = initialSize;
-	}
-
-	public int getMinIdle() {
-		return minIdle;
-	}
-
-	public void setMinIdle(int minIdle) {
-		this.minIdle = minIdle;
-	}
-
-	public int getMaxActive() {
-		return maxActive;
-	}
-
-	public void setMaxActive(int maxActive) {
-		this.maxActive = maxActive;
-	}
-
-	public long getTimeBetweenEvictionRunsMillis() {
-		return timeBetweenEvictionRunsMillis;
-	}
-
-	public void setTimeBetweenEvictionRunsMillis(long timeBetweenEvictionRunsMillis) {
-		this.timeBetweenEvictionRunsMillis = timeBetweenEvictionRunsMillis;
-	}
-
-	public long getMinEvictableIdleTimeMillis() {
-		return minEvictableIdleTimeMillis;
-	}
-
-	public void setMinEvictableIdleTimeMillis(long minEvictableIdleTimeMillis) {
-		this.minEvictableIdleTimeMillis = minEvictableIdleTimeMillis;
-	}
-
-	public String getValidationQuery() {
-		return validationQuery;
-	}
-
-	public void setValidationQuery(String validationQuery) {
-		this.validationQuery = validationQuery;
-	}
-
-	public boolean isTestWhileIdle() {
-		return testWhileIdle;
-	}
-
-	public void setTestWhileIdle(boolean testWhileIdle) {
-		this.testWhileIdle = testWhileIdle;
-	}
-
-	public boolean isTestOnBorrow() {
-		return testOnBorrow;
-	}
-
-	public void setTestOnBorrow(boolean testOnBorrow) {
-		this.testOnBorrow = testOnBorrow;
-	}
-
-	public boolean isTestOnReturn() {
-		return testOnReturn;
-	}
-
-	public void setTestOnReturn(boolean testOnReturn) {
-		this.testOnReturn = testOnReturn;
-	}
-
-	public boolean isPoolPreparedStatements() {
-		return poolPreparedStatements;
-	}
-
-	public void setPoolPreparedStatements(boolean poolPreparedStatements) {
-		this.poolPreparedStatements = poolPreparedStatements;
-	}
-
-	public int getMaxPoolPreparedStatementPerConnectionSize() {
-		return maxPoolPreparedStatementPerConnectionSize;
-	}
-
-	public void setMaxPoolPreparedStatementPerConnectionSize(int maxPoolPreparedStatementPerConnectionSize) {
-		this.maxPoolPreparedStatementPerConnectionSize = maxPoolPreparedStatementPerConnectionSize;
-	}
-
-	public String getFilters() {
-		return filters;
-	}
-
-	public void setFilters(String filters) {
-		this.filters = filters;
-	}
-
-	public String getConnectionProperties() {
-		return connectionProperties;
-	}
-
-	public void setConnectionProperties(String connectionProperties) {
-		this.connectionProperties = connectionProperties;
 	}
 }

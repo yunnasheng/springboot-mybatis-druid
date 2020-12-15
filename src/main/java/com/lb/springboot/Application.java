a538511c62869776ee1978769da2bcd9934a7d02
yunnasheng <yunnasheng@payeasenet.com>
2020-12-15 09:34:49 +0800

    add logback

--- src/main/java/com/lb/springboot/Application.java
+++ src/main/java/com/lb/springboot/Application.java
@@ -2,12 +2,33 @@ package com.lb.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.lb.springboot.config.AppConfig;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext ctx = SpringApplication.run(Application.class, args);
+		// 测试Logback打印配置
+		AppConfig appConfig = ctx.getBean(AppConfig.class);
+		appConfig.outprint();
+	}
+
+	/**
+	 * 测试接口
+	 * 
+	 * @param name
+	 * @return
+	 */
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
